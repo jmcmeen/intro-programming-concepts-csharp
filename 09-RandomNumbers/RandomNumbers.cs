3bf1927a964a121900bc3a859bd664a791dae3a3
John McMeen <jnmcmeen@northeaststate.edu>
2022-11-08 13:42:42 -0500

    Quite literally, some Random stuff, and multidimensional arrays

---
+++ 09-RandomNumbers/RandomNumbers.cs
@@ -0,0 +1,30 @@
+﻿namespace CISP1010
+{
+    internal class RandomNumbers
+    {
+        static void Main(string[] args)
+        {
+            //create a object of type Random
+            Random r = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"The next random in is {r.Next()}");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(RollDice(6));
+            }
+        }
+
+        static uint RollDice(uint numSides)
+        {
+            Random r = new Random();
+            int temp = r.Next(1, Convert.ToInt32(numSides));
+            uint output = Convert.ToUInt32(temp);
+            return output;
+        }
+
+    }
+}
---
+++ 10-MultidimensionalArrays/MultidimensionalArrays.cs
@@ -0,0 +1,32 @@
+﻿namespace _10_MultidimensionalArrays
+{
+    internal class MultidimensionalArrays
+    {
+        static void Main(string[] args)
+        {
+            int[,] scores = new int[5,10];
+
+            
+            Random r = new Random();
+
+
+            for(int i = 0; i < scores.GetLength(0); i++)
+            {
+                for(int j = 0; j < scores.GetLength(1); j++)
+                {
+                    scores[i, j] = r.Next(1,7);
+                }
+            }
+
+
+            for (int i = 0; i < scores.GetLength(0); i++)
+            {
+                for (int j = 0; j < scores.GetLength(1); j++)
+                {
+                    Console.Write(scores[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
