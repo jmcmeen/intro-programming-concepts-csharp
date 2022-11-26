9be389c7bfb46ee878a5155d8413d5bb9e8e0c42
John McMeen <jnmcmeen@northeaststate.edu>
2022-11-26 15:53:21 -0500

    fixing some typos in comments

--- 12-ReferenceTypes/ReferenceTypes.cs
+++ 12-ReferenceTypes/ReferenceTypes.cs
@@ -25,14 +25,14 @@ namespace CISP1010
             //Reference types require you to allocate memory with the new keyword
             person = new Person("Burf", "Snerfle", 20);
 
-            //Now we have a memory location called person, that holds a refernce to another memory location
-            //where the Person object's attributes are stored. Since C# is a memory managed lanaguge,
+            //Now we have a memory location called person, that holds a reference to another memory location
+            //where the Person object's attributes are stored. Since C# is a memory managed language,
             //if you reuse the person reference, such as in the code below, the old memory containing Burf Snerfle
-            //is automatically garbage collected (deleted/deallocated), and replaced with new memory for the
+            //is automatically garbage collected (deleted/deallocated), and replaced with new memory (allocated) for the
             //new Person object
             person = new Person("Lucy", "Goosey", 56);
 
-            //Once must take care when using refernce types and the assignment operator (=)
+            //One must take care when using refernce types and the assignment operator (=)
             //Let's look at value types again
             int a = 12;
             int b = 0;
@@ -44,8 +44,8 @@ namespace CISP1010
             Console.WriteLine($"a = {a}, b = {b}");
 
             //So the same thing happens with reference types, except what a reference type stores is a memory
-            //address, so when you use the assignment operator, you copy an address in memory and now two object 
-            //reference point to the same memory location. The example below is not allocating new memory for
+            //address, so when you use the assignment operator, you copy an address in memory and now two
+            //references point to the same object. The example below is not allocating new memory for
             //personB, but rather now both personA and personB point to the same place in memory
             Person personA = new Person("Douglas", "Adams", 42);
             Person personB = personA;
@@ -53,8 +53,8 @@ namespace CISP1010
             Console.WriteLine($"{personA.firstName} {personA.lastName} {personA.age}");
             Console.WriteLine($"{personB.firstName} {personB.lastName} {personB.age}");
 
-            //If you make a change to personA, they are reflected in the personB reference, because they are not
-            //two objects, but rather two references to the same object
+            //If you make a changes to personA, they are reflected in the personB reference, because they are not
+            //two objects, but rather two references pointing to the same object
             personA.firstName = "Not";
             personA.lastName = "New Memory";
 
