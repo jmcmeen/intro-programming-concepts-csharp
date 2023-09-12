b8a4ff8fa985cda780619afdc8929d97619e86b7
Northeast State CIS <jnmcmeen@northeaststate.edu>
2023-09-12 00:40:46 -0400

    Beep()

---
+++ 01-HelloBeep/HelloBeep.cs
@@ -0,0 +1,21 @@
+﻿/// File: HelloBeep.cs
+/// Name: John McMeen
+/// Email: jnmcmeen@northeaststate.edu
+/// Project: C# Examples
+namespace CISP1010
+{
+    /// <summary>
+    /// It literally just beeps. That is all.
+    /// </summary>
+    internal class HelloBeep
+    {
+        /// <summary>
+        /// Program entry point
+        /// </summary>
+        static void Main()
+        {
+            //Make it beep
+            Console.Beep();
+        }
+    }
+}
