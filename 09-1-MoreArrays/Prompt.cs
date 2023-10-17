ec596aacda9acd0ad15d2e4e18b8e2415eeb1754
Northeast State CISP <jnmcmeen@northeaststate.edu>
2023-10-17 15:28:18 -0400

    Moved some examples around

---
+++ 09-1-MoreArrays/Prompt.cs
@@ -0,0 +1,45 @@
+﻿/// Name: John McMeen
+/// Email: jnmcmeen@northeaststate.edu
+/// Project: C# Examples
+
+namespace _09_1_MoreArrays
+{
+    public static class Prompt
+    {
+        /// <summary>
+        /// Prompts the user for an int value and returns it
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static int ForInt(string message)
+        {
+            Console.Write(message);
+            string? input = Console.ReadLine();
+            return Convert.ToInt32(input);
+        }
+
+        /// <summary>
+        /// Prompts the user for a float value and returns it
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static float ForFloat(string message)
+        {
+            Console.Write(message);
+            string? input = Console.ReadLine();
+            return Convert.ToSingle(input);
+        }
+
+        /// <summary>
+        /// Prompts the user for an double value and returns it
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static double ForDouble(string message)
+        {
+            Console.Write(message);
+            string? input = Console.ReadLine();
+            return Convert.ToDouble(input);
+        }
+    }
+}
