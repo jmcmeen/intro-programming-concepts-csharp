6bdab909dc6ca0d88b017526077747e0a7b9d9f2
Northeast State CISP <jnmcmeen@northeaststate.edu>
2023-11-16 16:02:46 -0500

    Cleaning things up a bit @#(#$&!

--- 08-1-CommandLineArguments/Program.cs
+++ 08-1-CommandLineArguments/Program.cs
@@ -20,16 +20,16 @@ namespace _08_1_CommandLineArguments
             //array for above {"-c", "42", "3". "hello"}
 
             //see if arg array is empty
-            if(args.Length != 0)
+            if(args.Length == 0)
+            {
+                Console.WriteLine("No command line arguments specified!");
+            }
+            else
             {
                 foreach (string arg in args)
                 {
                     Console.WriteLine($"  {arg}");
                 }
-            }
-            else //if not print
-            {
-                Console.WriteLine("No command line arguments specified!");
             }
         }
     }
